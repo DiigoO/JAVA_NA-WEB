da6ab049c9183c771e72201948a6e00ee40161e4
logonrm <logonrm@fiap.com.br>
2017-08-23 20:37:20 -0300

    23082017

---
+++ Digo/JAVA_NA_WEB/JNW-Spring-MVC/src/fiap/scj/springmvc/bean/Aluno.java
@@ -0,0 +1,25 @@
+package fiap.scj.springmvc.bean;
+
+public class Aluno {
+	private String nome;
+	private String dataNasc;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", dataNasc=" + dataNasc + "]";
+	}
+	
+}
